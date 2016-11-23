0e6ab7b4e4cb02c8489cbd81cf73185c470324a1
tu <tu0703@gmail.com>
2016-11-23 16:11:14 +0700

    get setting variables from environt

--- bookzen_flask/settings.py
+++ bookzen_flask/settings.py
@@ -1,10 +1,12 @@
+import os
+
 MONGODB_SETTINGS = {
         'db': 'bookzen',
         'host': 'mongodb://localhost:27017/'}
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = "Ringa Linga"
+SECRET_KEY = os.environ.get("SECRET_KEY", "Ringa Linga")
 
-MY_EMAIL_ADDRESS = ""
-EMAIL_ACCOUNT = ""
-EMAIL_PASSWORD = ""
+MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "")
+EMAIL_ACCOUNT = os.environ.get("EMAIL_ACCOUNT", "")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD", "")
