d06d0126331011522686725f00df4fc45d938d2e
tu <tu0703@gmail.com>
2016-11-28 13:20:33 +0700

    revert script

--- bookcrawl/run_spiders.py
+++ bookcrawl/run_spiders.py
@@ -1,13 +1,12 @@
 import logging
 from twisted.internet import reactor
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
 from bookcrawl.spiders.tiki import TikiSpider
 from bookcrawl.spiders.lazada import LazadaSpider
 from bookcrawl.spiders.vinabook import VinabookSpider
-from bookcrawl.spiders.fahasa import FahasaSpider
 
 configure_logging(install_root_handler=False)
 logging.basicConfig(
@@ -17,14 +16,11 @@ logging.basicConfig(
 )
 
 
-runner = CrawlerProcess(get_project_settings())
+runner = CrawlerRunner(get_project_settings())
 runner.crawl(TikiSpider)
 runner.crawl(LazadaSpider)
 runner.crawl(VinabookSpider)
-runner.start()
+d = runner.join()
+d.addBoth(lambda _: reactor.stop())
 
-process = CrawlerProcess({
-        'USER_AGENT': 'google-bot',
-        })
-process.crawl(FahasaSpider)
-process.start()
+reactor.run()
