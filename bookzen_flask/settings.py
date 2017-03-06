41a7b85633d5d79a780fe1da799990d05bc6a2a8
tu <tu0703@gmail.com>
2017-03-06 21:18:33 +0700

    new settings arguments

--- bookzen_flask/settings.py
+++ bookzen_flask/settings.py
@@ -10,3 +10,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "Ringa Linga")
 MY_EMAIL_ADDRESS = "youremail@gmail.com"
 EMAIL_ACCOUNT = "Gmailaccount"
 EMAIL_PASSWORD = "Gmailpass"
+
+INSTAGRAM_USER = ""
+INSTAGRAM_PASSWORD = ""
