0f4ccf05da846d7d614559417ee15a5868d13027
tu <tu0703@gmail.com>
2017-05-26 16:04:07 +0700

    fix FAHASA spider, using Splash now for bypass Cookies gen by JS

--- bookcrawl/bookcrawl/settings.py
+++ bookcrawl/bookcrawl/settings.py
@@ -1,99 +1,106 @@
-# -*- coding: utf-8 -*-
+#  -*- coding: utf-8 -*-
 
-# Scrapy settings for bookcrawl project
+#  Scrapy settings for bookcrawl project
 #
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
+#  For simplicity, this file contains only settings considered important or
+#  commonly used. You can find more settings consulting the documentation:
 #
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#      http://doc.scrapy.org/en/latest/topics/settings.html
+#      http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#      http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'bookcrawl'
 
 SPIDER_MODULES = ['bookcrawl.spiders']
 NEWSPIDER_MODULE = 'bookcrawl.spiders'
 
+SPLASH_URL = 'http://localhost:8050/'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'bookcrawl (+http://www.yourdomain.com)'
-# USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
+#  Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = 'bookcrawl (+http://www.yourdomain.com)'
+#  USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+#  Obey robots.txt rules
+ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+#  Configure maximum concurrent requests performed by Scrapy (default: 16)
+# CONCURRENT_REQUESTS = 32
 
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+#  Configure a delay for requests for the same website (default: 0)
+#  See http://scrapy.readthedocs.org/en/latest/topics/settings.html# download-delay
+#  See also autothrottle settings and docs
+# DOWNLOAD_DELAY = 3
+#  The download delay setting will honor only one of:
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+#  Disable cookies (enabled by default)
+COOKIES_ENABLED = True
+SPLASH_COOKIES_DEBUG = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+#  Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+#  Override the default request headers:
+# DEFAULT_REQUEST_HEADERS = {
+#    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#    'Accept-Language': 'en',
+# }
 
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'bookcrawl.middlewares.MyCustomSpiderMiddleware': 543,
-#}
+#  Enable or disable spider middlewares
+#  See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'bookcrawl.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+#  Enable or disable downloader middlewares
+#  See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+# DOWNLOADER_MIDDLEWARES = {
+#     'bookcrawl.middlewares.MyCustomDownloaderMiddleware': 543,
+# }
 DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
     'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
 }
 
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+#  Enable or disable extensions
+#  See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#     'scrapy.extensions.telnet.TelnetConsole': None,
+# }
 
-# Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'bookcrawl.pipelines.SomePipeline': 300,
-#}
+#  Configure item pipelines
+#  See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# ITEM_PIPELINES = {
+#     'bookcrawl.pipelines.SomePipeline': 300,
+# }
 ITEM_PIPELINES = {'bookcrawl.pipelines.MongoPipeline': 900, }
 
 MONGODB_URI = 'mongodb://localhost:27017'
 MONGODB_DB = "bookzen"
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+#  Enable and configure the AutoThrottle extension (disabled by default)
+#  See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
+#  The initial download delay
 AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
+#  The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
+#  The average number of requests Scrapy should be sending in parallel to
+#  each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
+#  Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = True
 
-# Enable and configure HTTP caching (disabled by default)
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+#  Enable and configure HTTP caching (disabled by default)
+#  See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html# httpcache-middleware-settings
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- bookcrawl/bookcrawl/spiders/fahasa.py
+++ bookcrawl/bookcrawl/spiders/fahasa.py
@@ -1,39 +1,92 @@
 # -*- coding: utf-8 -*-
 import datetime
 import socket
+from urllib import urlencode
 import re
 
+import scrapy
+from scrapy.http import Request
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, Join
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy_splash import SplashRequest
 
 from unidecode import unidecode
 from bookcrawl.items import BooksItem
 
+script = """
+function main(splash)
+    splash:init_cookies(splash.args.cookies)
+    local url = splash.args.url
+    assert(splash:go(url))
+    assert(splash:wait(5))
 
-class FahasaSpider(CrawlSpider):
+    return {
+        cookies = splash:get_cookies(),
+        html = splash:html()
+    }
+end
+"""
+
+script2 = """
+function main(splash)
+    splash:init_cookies(splash.args.cookies)
+    local url = splash.args.url
+    assert(splash:go(url))
+    assert(splash:wait(0.5))
+
+    return {
+        cookies = splash:get_cookies(),
+        html = splash:html()
+    }
+end
+"""
+
+
+class FahasaSpider(scrapy.Spider):
     name = 'fahasa'
     allowed_domains = ['fahasa.com']
     start_urls = [
-            "http://www.fahasa.com/sach-trong-nuoc/van-hoc-trong-nuoc.html",
-            "http://www.fahasa.com/sach-trong-nuoc/van-hoc-dich.html",
-            "http://www.fahasa.com/sach-trong-nuoc/kinh-te-chinh-tri-phap-ly.html",
-            "http://www.fahasa.com/sach-trong-nuoc/tam-ly-ky-nang-song.html",
-            "http://www.fahasa.com/sach-trong-nuoc/kien-thuc-tong-hop.html",
-            "http://www.fahasa.com/sach-trong-nuoc/khoa-hoc-ky-thuat.html",
-            "http://www.fahasa.com/sach-trong-nuoc/tu-dien.html",
-            "http://www.fahasa.com/sach-trong-nuoc/sach-hoc-ngoai-ngu.html",
-            "http://www.fahasa.com/foreigncategory.html",
+            "https://www.fahasa.com/sach-trong-nuoc/van-hoc-trong-nuoc.html",
+            "https://www.fahasa.com/sach-trong-nuoc/van-hoc-dich.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/kinh-te-chinh-tri-phap-ly.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/tam-ly-ky-nang-song.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/kien-thuc-tong-hop.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/khoa-hoc-ky-thuat.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/tu-dien.html",
+            # "https://www.fahasa.com/sach-trong-nuoc/sach-hoc-ngoai-ngu.html",
+            # "https://www.fahasa.com/foreigncategory.html",
             ]
 
-    rules = (
-        Rule(LinkExtractor(
-            restrict_xpaths='//*[@title="Next"]')),
-        Rule(LinkExtractor(
-            restrict_xpaths='//*[@class="product-name p-name-list"]/a'),
-            callback='parse_item'),
-    )
+    # rules = (
+    #     Rule(LinkExtractor(
+    #         restrict_xpaths='//*[@title="Next"]'),
+    #         process_links='process_links'),
+    #     Rule(LinkExtractor(
+    #         restrict_xpaths='//*[@class="product-name p-name-list"]/a'),
+    #         process_links='process_links',
+    #         callback='parse_item'),
+    # )
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, endpoint='execute',
+                                args={'lua_source': script})
+
+    def parse(self, response):
+        # Get the next page and yield Request
+        next_selector = response.xpath('//*[@title="Next"]/@href')
+        for url in next_selector.extract():
+            yield SplashRequest(url, endpoint='execute',
+                                args={'lua_source': script2})
+
+        # Get URL in page and yield Request
+        url_selector = response.xpath(
+            '//*[@class="product-name p-name-list"]/a/@href')
+        for url in url_selector.extract():
+            yield SplashRequest(url, callback=self.parse_item,
+                                endpoint='execute',
+                                args={'lua_source': script2})
 
     def parse_item(self, response):
         """
@@ -44,9 +97,12 @@ class FahasaSpider(CrawlSpider):
         """
         l = ItemLoader(item=BooksItem(), response=response)
 
-        l.add_value('name', l.get_xpath('//*[@class="product-name"]/h1/text()')[-1])
-        l.add_value('name_unidecode', unidecode(l.get_xpath('//*[@class="product-name"]/h1/text()')[-1]))
-        l.add_xpath('price', '//*[@class="price-box"]/p[1]/span[2]/text()', TakeFirst(), re=r'\d+\.\d+')
+        l.add_value('name', l.get_xpath(
+                                '//*[@class="product-name"]/h1/text()')[-1])
+        l.add_value('name_unidecode', unidecode(l.get_xpath(
+                                '//*[@class="product-name"]/h1/text()')[-1]))
+        l.add_xpath('price', '//*[@class="price-box"]/p[1]/span[2]/text()',
+                    TakeFirst(), re=r'\d+\.\d+')
         l.add_value('description',
                     filter(None,
                            [re.sub('<[^<]+?>', '', i) for i in l.get_xpath('//*[@class="std"]')]),
